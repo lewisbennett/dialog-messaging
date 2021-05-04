cfb0fbc307b74d8bc37cf4bb59c157dc8f6eb452
Lewis <lewisbennett669@gmail.com>
2021-05-04 17:10:58 +0100

    Update MainViewModel.cs

--- samples/Sample.MvvmCross.Core/MainViewModel.cs
+++ samples/Sample.MvvmCross.Core/MainViewModel.cs
@@ -6,88 +6,41 @@ namespace Sample.MvvmCross.Core
 {
     public class MainViewModel : MvxViewModel
     {
-        private IMessaging _messaging;
+        public IMvxCommand ActionSheetButtonClickCommand { get; set; }
 
-        public IMvxCommand ActionSheetButtonClickCommand { get; private set; }
+        public IMvxCommand ActionSheetBottomButtonClickCommand { get; set; }
 
-        public IMvxCommand ActionSheetBottomButtonClickCommand { get; private set; }
+        public IMvxCommand AlertButtonClickCommand { get; set; }
 
-        public IMvxCommand AlertButtonClickCommand { get; private set; }
+        public IMvxCommand ConfirmButtonClickCommand { get; set; }
 
-        public IMvxCommand ConfirmButtonClickCommand { get; private set; }
+        public IMvxCommand DeleteButtonClickCommand { get; set; }
 
-        public IMvxCommand DeleteButtonClickCommand { get; private set; }
+        public IMvxCommand LoadingButtonClickCommand { get; set; }
 
-        public IMvxCommand LoadingButtonClickCommand { get; private set; }
+        public IMvxCommand PromptButtonClickCommand { get; set; }
 
-        public IMvxCommand PromptButtonClickCommand { get; private set; }
+        public IMvxCommand SnackbarButtonClickCommand { get; set; }
 
-        public IMvxCommand SnackbarButtonClickCommand { get; private set; }
-
-        public IMvxCommand ToastButtonClickCommand { get; private set; }
-
-        private void ActionSheetButton_Click()
-        {
-            _messaging.ActionSheet();
-        }
-
-        private void ActionSheetBottomButton_Click()
-        {
-            _messaging.ActionSheetBottom();
-        }
-
-        private void AlertButton_Click()
-        {
-            _messaging.Alert();
-        }
-
-        private void ConfirmButton_Click()
-        {
-            _messaging.Confirm();
-        }
-
-        private void DeleteButton_Click()
-        {
-            _messaging.Delete();
-        }
-
-        private void LoadingButton_Click()
-        {
-            _messaging.Loading();
-        }
-
-        private void PromptButton_Click()
-        {
-            _messaging.Prompt();
-        }
-
-        private void SnackbarButton_Click()
-        {
-            _messaging.Snackbar();
-        }
-
-        private void ToastButton_Click()
-        {
-            _messaging.Toast();
-        }
+        public IMvxCommand ToastButtonClickCommand { get; set; }
 
         public override void Prepare()
         {
             base.Prepare();
 
             // Choose which method set to test.
-            _messaging = new SyncMessaging();
-            //_messaging = new AsyncMessaging();
+            var messaging = new SyncMessaging();
+            //var messaging = new AsyncMessaging();
 
-            ActionSheetButtonClickCommand = new MvxCommand(ActionSheetButton_Click);
-            ActionSheetBottomButtonClickCommand = new MvxCommand(ActionSheetBottomButton_Click);
-            AlertButtonClickCommand = new MvxCommand(AlertButton_Click);
-            ConfirmButtonClickCommand = new MvxCommand(ConfirmButton_Click);
-            DeleteButtonClickCommand = new MvxCommand(DeleteButton_Click);
-            LoadingButtonClickCommand = new MvxCommand(LoadingButton_Click);
-            PromptButtonClickCommand = new MvxCommand(PromptButton_Click);
-            SnackbarButtonClickCommand = new MvxCommand(SnackbarButton_Click);
-            ToastButtonClickCommand = new MvxCommand(ToastButton_Click);
+            ActionSheetButtonClickCommand = new MvxCommand(messaging.ActionSheet);
+            ActionSheetBottomButtonClickCommand = new MvxCommand(messaging.ActionSheetBottom);
+            AlertButtonClickCommand = new MvxCommand(messaging.Alert);
+            ConfirmButtonClickCommand = new MvxCommand(messaging.Confirm);
+            DeleteButtonClickCommand = new MvxCommand(messaging.Delete);
+            LoadingButtonClickCommand = new MvxCommand(messaging.Loading);
+            PromptButtonClickCommand = new MvxCommand(messaging.Prompt);
+            SnackbarButtonClickCommand = new MvxCommand(messaging.Snackbar);
+            ToastButtonClickCommand = new MvxCommand(messaging.Toast);
         }
     }
 }
