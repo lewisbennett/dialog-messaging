ef745ae6e79a447482252aa7a2c1d3b1ae7b6763
Lewis <lewisbennett669@gmail.com>
2021-05-07 11:49:38 +0100

    Re-added action sheet bottom config defaults

---
+++ src/DialogMessaging.Core/Platforms/Droid/Interactions/ActionSheetBottomConfig.cs
@@ -0,0 +1,50 @@
+﻿namespace DialogMessaging.Interactions
+{
+    public static partial class ActionSheetBottomConfigDefaults
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the default value for whether the dialog is cancelable.
+        /// </summary>
+        public static bool Cancelable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default value for the resource ID of the layout to use.
+        /// </summary>
+        public static int? LayoutResID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default value for the resource ID of the style to use.
+        /// </summary>
+        public static int? StyleResID { get; set; }
+        #endregion
+    }
+
+    public partial interface IActionSheetBottomConfig<TActionSheetItemConfig>
+    {
+    }
+
+    public partial class ActionSheetBottomConfig
+    {
+        #region Constructors
+        public ActionSheetBottomConfig()
+        {
+            Cancelable = ActionSheetConfigDefaults.Cancelable;
+            LayoutResID = ActionSheetConfigDefaults.LayoutResID;
+            StyleResID = ActionSheetConfigDefaults.StyleResID;
+        }
+        #endregion
+    }
+
+    public partial class ActionSheetBottomAsyncConfig
+    {
+        #region Constructors
+        public ActionSheetBottomAsyncConfig()
+        {
+            Cancelable = ActionSheetConfigDefaults.Cancelable;
+            LayoutResID = ActionSheetConfigDefaults.LayoutResID;
+            StyleResID = ActionSheetConfigDefaults.StyleResID;
+        }
+        #endregion
+    }
+}
---
+++ src/DialogMessaging.Core/Platforms/iOS/Interactions/ActionSheetBottomConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DialogMessaging.Interactions
+{
+    public static partial class ActionSheetBottomConfigDefaults
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the default <see cref="Type" /> of the custom view to use for the dialog, if any.
+        /// </summary>
+        public static Type CustomViewType { get; set; }
+        #endregion
+    }
+
+    public partial interface IActionSheetBottomConfig<TActionSheetItemConfig>
+    {
+    }
+
+    public partial class ActionSheetBottomConfig
+    {
+        #region Constructors
+        public ActionSheetBottomConfig()
+            : base()
+        {
+            CustomViewType = ActionSheetConfigDefaults.CustomViewType;
+        }
+        #endregion
+    }
+
+    public partial class ActionSheetBottomAsyncConfig
+    {
+        #region Constructors
+        public ActionSheetBottomAsyncConfig()
+            : base()
+        {
+            CustomViewType = ActionSheetConfigDefaults.CustomViewType;
+        }
+        #endregion
+    }
+}
